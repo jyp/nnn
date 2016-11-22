9ce45a68ee0e160528694f181dd5b6652bfc9423
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-11-22 15:14:21 +0100

    have list of parameters instead of just one

--- Tensors.hs
+++ Tensors.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds, TypeFamilies, PolyKinds, ScopedTypeVariables #-}
 
 import GHC.TypeLits
 import Data.Kind
+import Algebra.Classes
+import Prelude hiding (Num(..))
 
 data Pointer -- location in (GPU) memory. We assume that all values are (say) Floats for now.
   -- Todo: Add a parameter everywhere.
@@ -18,39 +21,48 @@ type Push d = N (Pull d)
 
 data Dim = Dim Nat Nat -- inner and outer dimensions
 
-data Tensor (p :: Nat) -- dimensions of parameters tensor ("free variables")
+data Grads (d :: Nat) (ps :: [Nat]) where
+  Nil :: Grads d '[]
+  (:>) :: Pull ('Dim p d) -> Grads d ps -> Grads d (p ': ps)
+
+instance Additive (Grads d ps) where
+
+mulGrads :: Grads e ps -> e×d -> Grads d ps
+mulGrads Nil _ = Nil
+mulGrads (g :> gs) m = g · m :> mulGrads gs m
+
+data Tensor (ps :: [Nat]) -- dimensions of parameter tensors ("free variables")
             (d :: Nat) -- dimensions of result.
   = Tensor {value :: Pull ('Dim 1 d) -- value
-           ,grad :: Pull ('Dim p d)  -- gradient wrt. the parameters p
+           ,grad :: Grads d ps  -- gradient wrt. the parameters ps
            }
-
-data Sing :: a -> Type
 
 idT :: Pull ('Dim d d)
 idT index pointer = error "if i = j then 1 else 0"
 
-
-parameters :: Pull ('Dim 1 p) -> Tensor p p -- input parameters in the computation.
-parameters x = Tensor x idT
+var :: Pull ('Dim 1 p) -> Tensor '[p] p -- input parameters in the computation.
+var x = Tensor x (idT :> Nil)
 
 
 (·) :: Pull ('Dim d e) -> Pull ('Dim e f) -> Pull ('Dim d f)
 (·) = error "matrix product"
 
 
--- VERY IMPORTANT NOTE: the parameters of the input (y) can't be
--- passed implicitly ("in a closure environment") to the body of "f".
--- Fortunately the typesystem will prevent this (because of the inner
--- quantification of "q").
+type a × b = Pull ('Dim a b)
 
--- This is not too annoying in a DL context, but we'd really want the
--- function "f" to introduce its own parameters. (The weights for the
--- neurons of the layers embedded there.) For simplicity, we may want to have another
--- set of parameters introduced at the same 
-chain :: forall p d e. Tensor p d -> (forall q. Tensor q d -> Tensor q e) -> Tensor p e
-chain (Tensor y dy) f = Tensor z (dy · dz)
-  where (Tensor z dz) = f (Tensor y idT)
-        z :: Pull ('Dim 1 e) -- final values
-        dz :: Pull ('Dim d e) -- the gradient of z wrt. the result of y
-        -- y :: Pull ('Dim 1 d) -- intermediate values
-        -- dy :: Pull ('Dim p d) -- gradient of y wrt. the original params ("intermediate gradient")
+-- chain' :: forall p d e r. Tensor '[p] d -> (forall q. Tensor '[q]  d -> Tensor (q ': r) e) -> Tensor (p ': r) e
+-- chain' (Tensor (y :: 1×d) ((dydp :: p×d) :> Nil)) f = case f (Tensor y (idT :> Nil)) of
+--   Tensor (z :: 1×e) (dpdy :> dzdr) -> Tensor z (dydp · dpdy :> dzdr)
+
+chain :: forall d e r. Tensor r d -> (forall q. Tensor (q : r)  d -> Tensor (q : r) e) -> Tensor r e
+chain (Tensor (y :: 1×d) dydr) f = case f (Tensor y (idT :> dydr)) of
+  Tensor (z :: 1×e) (dpdy :> dzdr) -> Tensor z (mulGrads dydr dpdy + dzdr)
+
+-- param1 :: 1×p -> Tensor [q] e -> Tensor [q,p] e
+-- param1 v t = 
+
+-- data Expr ps d where
+--   Bind :: Expr ps d -> (Expr ps d -> Expr ps e) -> Expr ps e
+--   Param0 :: Expr (d:ps) d
+--   Param1 :: Expr (p0:d:ps) d
+
