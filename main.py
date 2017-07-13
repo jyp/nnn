b16eec35e5ddc69bd8a43ba8166087c6ef8029db
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-07-13 22:04:05 +0200

    fixes in rts and example

---
+++ main.py
@@ -0,0 +1,28 @@
+import ttflow_rts
+import tensorflow as tf
+import numpy as np
+import ttflow_example
+import os
+
+os.environ["CUDA_DEVICE_ORDER"]= "PCI_BUS_ID"
+
+if os.environ.get("CUDA_VISIBLE_DEVICES") is None:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+model = ttflow_example.mkModel()
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def train_generator():
+    for _ in range(1000):
+        yield mnist.train.next_batch(100)
+    return
+
+def valid_generator():
+    return
+
+sess = tf.Session()
+
+ttflow_rts.train(sess,model,tf.train.AdamOptimizer(1e-4),train_generator,valid_generator,1)
--- ttflow_rts.py
+++ ttflow_rts.py
@@ -1,26 +1,26 @@
+import tensorflow as tf
 
 
 # optimize is one of tf.train.GradientDescentOptimizer(0.05), etc.
 def train (sess, model, optimizer, train_generator, valid_generator, epochs):
-    (x,y,y_,loss) = model
-    sess.run(init)
+    (x,y,y_,accuracy,loss) = model
+    sess.run(tf.global_variables_initializer())
     train = optimizer.minimize(loss)
     for i in range(epochs):
         totalLoss = 0
-        for (x_train,y_train) in data_generator:
+        for (x_train,y_train) in train_generator():
             (_,loss) = sess.run([train,loss], feed_dict={x:x_train, y:y_train})
             totalLoss += loss
         print ("Training Loss = ", totalLoss / float(epochs))
 
         totalLoss = 0
-        for (x_train,y_train) in valid_generator:
-            loss = sess.run([loss], feed_dict={x:x_train, y:y_train})
-        print ("Validation Loss = ", totalLoss / float(epochs))
+        for (x_train,y_train) in valid_generator():
+            loss,accur = sess.run([loss,accuracy], feed_dict={x:x_train, y:y_train})
+        print ("Validation Loss = ", totalLoss / float(epochs), " Accuracy = ", accuracy / float(epochs))
 
 
 def predict (sess, model, x_generator):
-    (x,y,y_,loss) = model
+    (x,y,y_,accuracy,loss) = model
     sess.run(init)
-    train = optimizer.minimize(loss)
     for i in range(epochs):
-        sess.run(y, feed_dict={x:x_train})
+        sess.run(y, feed_dict={x:x_generator})
