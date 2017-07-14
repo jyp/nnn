6df36a1f4a9e9e3c8c573de566c1e385cdc75b92
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-07-14 18:45:58 +0200

    report results correctly

--- ttflow_rts.py
+++ ttflow_rts.py
@@ -7,24 +7,29 @@ def train (sess, model, optimizer, train_generator, valid_generator, epochs):
     train = optimizer.minimize(loss) # must come before the initializer (this line creates variables!)
     sess.run(tf.local_variables_initializer())
     sess.run(tf.global_variables_initializer())
-    for i in range(epochs):
+    for e in range(epochs):
+        totalAccur = 0
         totalLoss = 0
         n = 0
-        for (x_train,y_train) in enumerate(train_generator()):
-            (_,lossAcc) = sess.run([train,loss], feed_dict={x:x_train, y:y_train})
+        for (x_train,y_train) in train_generator():
+            _,lossAcc,accur = sess.run([train,loss,accuracy], feed_dict={x:x_train, y:y_train})
             n+=1
             totalLoss += lossAcc
-        print ("Training Loss = ", totalLoss / float(n))
+            totalAccur += accur
+        print ("Training Loss = ", totalLoss / float(n), " Training accuracy = ", totalAccur / float(n))
 
         totalLoss = 0
         totalAccur = 0
         n = 0
-        for (n,(x_train,y_train)) in enumerate(valid_generator()):
+        for (x_train,y_train) in valid_generator():
             lossAcc,accur = sess.run([loss,accuracy], feed_dict={x:x_train, y:y_train})
             totalLoss += lossAcc
             totalAccur += totalAccur
             n+=1
-        print ("Validation Loss = ", totalLoss / float(n), " Accuracy = ", totalAccur / float(n))
+        if n > 0:
+            print ("Validation Loss = ", totalLoss / float(n), " Validation accuracy = ", totalAccur / float(n))
+        else:
+            print ("No validation data.")
 
 
 def predict (sess, model, x_generator):
